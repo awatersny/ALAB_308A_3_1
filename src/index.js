2be607ab8ec8ce7de624f97cbc773f4b8f9437a1
awatersny <awatersny@gmail.com>
2025-03-25 13:51:50 -0400

    Get user data with await

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   }
-  central(id).then(db => {
-    dbs[db](id).then(user => {
-      console.log(user)
-    })
-  })
+  const db = await central(id)
+  const user = await dbs[db](id)
+  console.log(user)
 }
 
 getUserData(1)
