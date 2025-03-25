f548ec0b4301000df41ca115704a42823482d88d
awatersny <awatersny@gmail.com>
2025-03-25 15:26:00 -0400

    Wrap in a try...catch block

--- src/index.js
+++ src/index.js
@@ -2,25 +2,44 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 async function getUserData(id) {
+  try {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   }
-  const db = await central(id)
-  const user = await dbs[db](id)
-  const userVault = await vault(id)
-  return{
-    id: id,
-    name: userVault.name,
-    username: user.username,
-    email: userVault.email,
-    address: userVault.address,
-    phone: userVault.phone,
-    website: user.website,
-    company: user.company
+    const db = await central(id)
+    const user = await dbs[db](id)
+    const userVault = await vault(id)
+    return{
+      id: id,
+      name: userVault.name,
+      username: user.username,
+      email: userVault.email,
+      address: userVault.address,
+      phone: userVault.phone,
+      website: user.website,
+      company: user.company
+    }
+  } catch (error) {
+    return error
   }
 }
+
+// function getUserData(id) {
+//   const data = {id: id}
+//   const dbs = {
+//     db1: db1,
+//     db2: db2,
+//     db3: db3
+//   }
+//   const user = central(id)
+//   .then(db => dbs[db](id)
+//   .then(user => user))
+//   const userVault = vault(id).then(data => data)
+//   console.log(userVault)
+//   return userVault
+// }
 
 async function getAllUserData() {
   const data = []
@@ -30,5 +49,5 @@ async function getAllUserData() {
   return data
 }
 
-const data = await getAllUserData()
-console.log(data)
+// const data = await getUserData(1)
+// console.log(data)
